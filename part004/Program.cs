d9009f709f5ded48cc301521efe307a63b772a04
Nick <nicknickspb@icloud.com>
2021-11-17 23:39:23 +0300

    Решение 21 не через метод

--- part004/Program.cs
+++ part004/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+/*bool Palindrom(int Check)
+{
+    string symbol = Convert.ToString(Check);
+    int Check1 = symbol[0];
+    int Check2 = symbol[1];
+    int Check3 = symbol[2];
+    int Check4 = symbol[3];
+    int Check5 = symbol[4];
+    if(Check1==Check5&&Check2==Check4) return true;
+    else return false;
+}
+int Rand = new Random().Next(10000,100000);
+Console.WriteLine(Rand);
+if(Palindrom(Rand)) System.Console.WriteLine("Палиндром!");
+else System.Console.WriteLine("Не палиндром!");
+*/
+ 
+// 22. Найти расстояние между точками в пространстве 2D/3D
+
+
+// Почувствуй себя мидлом
+// 23. Показать таблицу квадратов чисел от 1 до N 
+// 24. Найти кубы чисел от 1 до N
+// 25. Найти сумму чисел от 1 до А
+// 26. Возведите число А в натуральную степень B используя цикл
+// 27. Определить количество цифр в числе
+// 28. Подсчитать сумму цифр в числе
+// 29. Написать программу вычисления произведения чисел от 1 до N
+// 30. Показать кубы чисел, заканчивающихся на четную цифру
